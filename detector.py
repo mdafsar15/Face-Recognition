f40f025a4b88d8ce1121207fe8c10ae3e15b883f
Md Afsar Ansari <57721986+mdafsar15@users.noreply.github.com>
2020-02-17 12:22:16 +0530

    Added Project

---
+++ detector.py
@@ -0,0 +1,26 @@
+import cv2 
+import numpy as np
+import os
+from PIL import Image
+faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml');
+cam=cv2.VideoCapture(0);
+recognizer=cv2.face.LBPHFaceRecognizer_create();
+recognizer.save("recognizer\\trainningData.yml")
+id=0
+font= cv2.FONT_HERSHEY_COMPLEX_SMALL
+while(True):
+    ret,img=cam.read();
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces=faceDetect.detectMultiScale(gray,1.3,5);
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        id,conf=recognizer.train(gray[y:y+h,x:x+w])
+        cv2.cv.PutText(cv2.cv.fromarray(img),str(id),(x,y+h),font,255);
+        
+    cv2.imshow("Face",img);
+    if(cv2.waitKey(1)==ord('q')):
+        break;
+cam.release()
+cv2.destroyAllWindows()
+
+    
